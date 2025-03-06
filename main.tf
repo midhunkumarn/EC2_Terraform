1c1c8c644e829ccf24042f7258f6ae4450077e77
MIDHUN KUMAR N <130952023+midhunkumarn@users.noreply.github.com>
2025-03-07 00:20:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "this" {
-  ami                     = "ami-04b4f1a9cf54c11d0"
-  instance_type           = "t2.micro"
-  subnet_id = "subnet-01db1530ad65f4232"
-  key_name = "ec2"
+  ami                     = var.ami_value
+  instance_type           = var.instance_type_value
+  subnet_id = var.subnet_id_value
+  key_name = var.key_name_value
 }
